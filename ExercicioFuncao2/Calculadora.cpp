188ea18bdf31d73c41d4eef06cfb0687024a4a9e
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-04-17 01:28:47 -0300

    Função para retorno de valores de todas as operação feita.

--- ExercicioFuncao2/Calculadora.cpp
+++ ExercicioFuncao2/Calculadora.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 
-double calcNumber(float, float);
+void calcNumber(float, float);
 
 int main()
 {
 	setlocale(0, "portuguese");
 
+	float Numb1, Numb2;
+
+	std::cout << "Insira o primeiro número: ";
+	std::cin >> Numb1;
+	std::cout << "\n";
+
+	std::cout << "Insira o segundo número: ";
+	std::cin >> Numb2;
+	std::cout << "\n";
+
+	std::cout << "Os resultados das operações são: \n";
+	calcNumber(Numb1, Numb2);
+
 	return 0;
+}
+
+void calcNumber(float Numb1, float Numb2)
+{
+	const double soma = Numb1 + Numb2;
+	const double subtracao = Numb1 - Numb2;
+	const double multiplicacao = Numb1 * Numb2;
+	const double divisao = Numb1 / Numb2;
+
+	std::cout << "Soma: " << soma << std::endl;
+	std::cout << "Subtração: " << subtracao << std::endl;
+	std::cout << "Multiplicação: " << multiplicacao << std::endl;
+	std::cout << "Divisão: " << divisao << std::endl;
+
 }
