ddc0c47cebedbe4716e1d00c4097c50228a4c68d
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-04-17 01:03:49 -0300

    Função para verificar se o número inserido é impar ou par!

--- ExercicioFuncao/FuncaoParImpar.cpp
+++ ExercicioFuncao/FuncaoParImpar.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+bool isPar(int Numb);
+
 int main()
 {
+	setlocale(0, "portuguese");
 
+	float Numb;
+
+	std::cout << "Insira um número para verificação: ";
+	std::cin >> Numb;
+	std::cout << "\nNúmero inserido foi: " << Numb << "\n";
+
+	if (isPar(Numb))
+	{
+		std::cout << "O número selecionado é par!" << std::endl;
+	}
+	else
+	{
+		std::cout << "O número selecionado é ímpar!" << std::endl;
+	}
+
+	return 0;
+}
+
+bool isPar(int Numb)
+{
+	int verify = Numb % 2;
+
+	if (verify == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
