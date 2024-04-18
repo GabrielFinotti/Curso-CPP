bc2c5ea087fc4ce7ac9988dffc2a106b28b2c004
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-04-17 22:58:54 -0300

    Utilizando o mesmo conceito de troca de dados com referência, porém utilizando ponteiros.

--- PassagemDeArgumentosPorValorPorReferencias/PassagemDeArgumentosPorValorPorReferencias.cpp
+++ PassagemDeArgumentosPorValorPorReferencias/PassagemDeArgumentosPorValorPorReferencias.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void TrocaNumeros(int&, int&);
+void TrocaNumeros2(int*, int*);
 
 int main()
 {
@@ -15,16 +16,28 @@ int main()
 
 	TrocaNumeros(number1, number2);
 
-	std::cout << "Valores depois da troca do número1 " << number1 << " e do número 2 " << number2 << std::endl;
+	std::cout << "Valores depois da troca do número1 " << number1 << " e do número 2 " << number2 << " utilizando a primeira função com referências!" << std::endl;
+
+	TrocaNumeros2(&number1, &number2);
+
+	std::cout << "Valores depois da troca do número1 " << number1 << " e do número 2 " << number2 << " utilizando a segunda função com ponteiros!" << std::endl;
+
 
 	system("pause");
 
 	return 0;
 }
 
-void TrocaNumeros(int &number1, int &number2)
+void TrocaNumeros(int& number1, int& number2)
 {
 	int temp{ number1 };
 	number1 = number2;
 	number2 = temp;
+}
+
+void TrocaNumeros2(int* number1, int* number2)
+{
+	int temp{ *number1 };
+	*number1 += *number2;
+	*number2 = temp + 100;
 }
