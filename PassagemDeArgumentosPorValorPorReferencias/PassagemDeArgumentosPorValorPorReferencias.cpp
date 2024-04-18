b6dcbeaeec4241573ef7a20d2fc0c0078c19cb4a
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-04-17 22:50:53 -0300

    Passando dados e valores utilizando refenrências em função sem precisar criar novas variáveis.

---
+++ PassagemDeArgumentosPorValorPorReferencias/PassagemDeArgumentosPorValorPorReferencias.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void TrocaNumeros(int&, int&);
+
+int main()
+{
+	setlocale(0, "portuguese");
+	int number1, number2;
+
+	std::cout << "Digite o primeiro número: ";
+	std::cin >> number1;
+
+	std::cout << "Digite o segundo número: ";
+	std::cin >> number2;
+
+	TrocaNumeros(number1, number2);
+
+	std::cout << "Valores depois da troca do número1 " << number1 << " e do número 2 " << number2 << std::endl;
+
+	system("pause");
+
+	return 0;
+}
+
+void TrocaNumeros(int &number1, int &number2)
+{
+	int temp{ number1 };
+	number1 = number2;
+	number2 = temp;
+}
