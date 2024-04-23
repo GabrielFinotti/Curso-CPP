a5f0adec02ad8f75e5af213f12123bf981266d5b
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-04-22 23:34:16 -0300

    Entendendo o conceito de classes, objetos e encapsulamento!

---
+++ OQueEClasse/CriandoClasse.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+class Casa
+{
+private:
+	int numQuartos{ 4 };
+	float size{ 90.0f };
+	bool btemSuite{ true };
+	
+public:
+	void showSize() const;
+	int obterNumQuartos() const;
+	bool TemSuite() const;
+	void setNumQuartos(int);
+};
+
+int main()
+{
+	setlocale(0, "portuguese");
+
+	Casa casaDePraia;
+	std::cout << "Tamanho da casa: "; 
+	casaDePraia.showSize();
+	std::cout << "\n";
+
+	std::cout << "Quantidade de quartos: " << casaDePraia.obterNumQuartos() << "\n";
+	std::cout << "Tem suite? " << casaDePraia.TemSuite() ? std::cout << "Sim" : std::cout << "Não";
+	casaDePraia.setNumQuartos(3);
+	std::cout << std::endl;
+
+	system("pause");
+
+	return 0;
+}
+
+void Casa::showSize() const
+{
+	std::cout << size;
+}
+
+int Casa::obterNumQuartos() const
+{
+	return numQuartos;
+}
+
+bool Casa::TemSuite() const
+{
+	return btemSuite;
+}
+
+void Casa::setNumQuartos(int num)
+{
+	if ((num <= 6) && (num >= 0))
+	{
+		numQuartos = num;
+	}
+	else
+	{
+		std::cout << "\nNúmero de quartos inválidos!";
+	}
+}
